62bae3261592ed1bacbfd0031811d3ffd9ef5b65
stevenhu1216 <stevenhu2323@gmail.com>
2020-10-23 21:28:27 +0800

    first

---
+++ CH2_lab1-2/source/han_xin.h
@@ -0,0 +1,5 @@
+int han_xin_1();
+int han_xin_2();
+int han_xin_3();
+int han_xin_4();
+int han_xin_5();
---
+++ CH2_lab1-2/source/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "han_xin.h"
+
+int main(void)
+{
+	 han_xin_1();
+	 han_xin_2();
+	 han_xin_3();
+	 han_xin_4();
+	 han_xin_5();
+
+	 printf("\n");
+	 system("PAUSE");
+
+}
